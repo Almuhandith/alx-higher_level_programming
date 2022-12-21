806276c2eb3375f891c7f971e0402c10ee72f947
Abdulsamad <almuhandith1497@gmail.com>
2022-12-21 11:01:37 +0100

    classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Empty Class Square"""
+"""Class Square with size"""
 
 
 class Square:
-    """Empty Class Square"""
-    pass
+    """Class to create a square with it size"""
+
+    def __init__(self, square_size):
+        """Constructor of the Square"""
+        self.__size = square_size
