a1f471b3f4b764017b8e3774817c1efced3e6223
Abdulsamad <almuhandith1497@gmail.com>
2023-01-10 17:51:58 +0100

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 """Module that contains a function that reads a file"""
 
 def read_file(filename=""):
-    with open('filename', encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+    with open('filename', 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
