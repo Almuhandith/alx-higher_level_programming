b556893d195f4d240944033726cc9d98f2ad35e3
Abdulsamad <almuhandith1497@gmail.com>
2023-01-10 22:24:22 +0100

    create list of arguments

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module that contains a function that writes a string to a text file """
 
+
 def write_file(filename="", text=""):
     """ Function that writes to a text file
     Args:
