1c0643070bccac3f3ef8dfaa3eeb4cb1bb8fe80d
Abdulsamad <almuhandith1497@gmail.com>
2023-04-05 04:24:52 +0100

    few fixes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Algorithms for list of integers."""
+"""Algorithms for array of integers."""
 
 
-def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+def find_peak(array_of_integers):
+    """Finds a peak in a array of unsorted integers."""
+    if array_of_integers:
+        array_of_integers.sort(reverse=True)
+        return array_of_integers[0]
