1d0c6fe720d9e8e6f22cd08b2f2396e480a70258
Abdulsamad <almuhandith1497@gmail.com>
2023-04-25 18:35:14 +0100

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,8 @@
 const fs = require('fs');
 
 try {
-	const content = fs.readFileSync(process.argv[2], 'utf-8');
-	console.log(content);
+  const content = fs.readFileSync(process.argv[2], 'utf-8');
+  console.log(content);
 } catch (error) {
-	console.error(error);
+  console.error(error);
 }
-
